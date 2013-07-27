3c4b2092bc9abde297d7c8b3caca6e0e837a5930
Prem Muthedath <prem.muthedath@gmail.com>
2013-07-27 21:01:14 +0530

    Bowling now advances Frame -- BIG breakthrough!

--- src/main/bowling/Bowling.java
+++ src/main/bowling/Bowling.java
@@ -3,23 +3,24 @@ package bowling;
 public class Bowling {
     private java.util.ArrayList <Frame> frames=new java.util.ArrayList<Frame>();
 
-    public void roll(int pins) {
-        if(frames.size()==0) advance();                
-        count(pins, frames.size());
+    public void roll(int pins)  {
+        if(frames.size()==0) advance(frames.size()-1);                        
+        for(int frame=0; frame < frames.size(); frame++)
+            if(!frames.get(frame).roll(pins)) advance(frame);
     }
 
-    public void advance() {
-        if(notDone()) frames.add(FrameFactory.create(this));        
+    private void advance(int frame) {
+        if(notCurrentFrame(frame)) return;
+        if(notDone()) frames.add(new FrameFactory().create());                
+    }
+
+    private boolean notCurrentFrame(int frame) {
+        return frame!=frames.size()-1;
     }
 
     private boolean notDone() {
         return !Tenpin.TOTAL_FRAMES.equals(frames.size());
     }   
-
-    private void count(int pins, int currentFrame)  {
-        for(int frame=0; frame < currentFrame; frame++)
-            frames.get(frame).count(pins);  
-    }
 
     public int score() {
         return score(frames.size());
