4a8add970a11245cebb9db2c8640420c2171058f
Prem Muthedath <prem.muthedath@gmail.com>
2013-07-27 21:26:29 +0530

    Some small cleanups

--- src/main/bowling/Bowling.java
+++ src/main/bowling/Bowling.java
@@ -3,19 +3,21 @@ package bowling;
 public class Bowling {
     private java.util.ArrayList <Frame> frames=new java.util.ArrayList<Frame>();
 
+    public Bowling() {
+        advance(frames.size()-1);
+    }
+
     public void roll(int pins)  {
-        if(frames.size()==0) advance(frames.size()-1);                        
         for(int frame=0; frame < frames.size(); frame++)
             if(!frames.get(frame).roll(pins)) advance(frame);
     }
 
     private void advance(int frame) {
-        if(notCurrentFrame(frame)) return;
-        if(notDone()) frames.add(new FrameFactory().create());                
+        if(currentFrame(frame) && notDone()) frames.add(new FrameFactory().create());                
     }
 
-    private boolean notCurrentFrame(int frame) {
-        return frame!=frames.size()-1;
+    private boolean currentFrame(int frame) {
+        return frame==frames.size()-1;
     }
 
     private boolean notDone() {
