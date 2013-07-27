eff8ed801d6f97bed68402bfe4b8d5bef3355d91
Prem Muthedath <prem.muthedath@gmail.com>
2013-07-27 22:07:55 +0530

    New class that intializes and creates a Bowling object

---
+++ src/main/bowling/configuration/Factory.java
@@ -0,0 +1,23 @@
+package bowling.configuration;
+
+import bowling.states.FirstBall;
+import bowling.core.Bowling;
+import bowling.core.Frame;
+import bowling.core.State;
+
+public class Factory {
+	public Bowling create() {
+		Bowling bowling=new Bowling();
+		bowling.add(frames());
+		return bowling;
+	}
+
+	private Frame[] frames() {
+    	Frame[] frames=new Frame[10];
+    	for(int i=0; i < frames.length; i++) {
+			State state=new FirstBall();
+			frames[i]=new Frame(state);
+    	}
+    	return frames;
+	}
+}
