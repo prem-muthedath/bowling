cadb607639b33978d60780e8c2561c52a78fa915
Prem Muthedath <prem.muthedath@gmail.com>
2013-07-27 22:06:58 +0530

    Moved out intialization logic.  Changed package.

---
+++ src/main/bowling/core/Bowling.java
@@ -0,0 +1,25 @@
+package bowling.core;
+
+ public class Bowling {
+    private Frame[] frames=new Frame[0];
+
+    public void add(Frame[] frames) {
+        this.frames=java.util.Arrays.copyOfRange(frames, 0, 10);
+    }
+
+    public void roll(int pins)  {
+        for(Frame each : frames)
+            if(each.roll(pins)) return;
+    }
+
+    public int score() {
+        return score(frames.length);
+    }
+
+    public int score(int aFrame) {
+        int score=0;
+        for(int frame=0; frame < Math.min(aFrame, frames.length); frame++)
+            score+=frames[frame].score();
+        return score;
+    }
+}
