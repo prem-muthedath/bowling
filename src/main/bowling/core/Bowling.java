2be7e24969d5b54483d7ab49b5c442c263e94c89
Prem Muthedath <prem.muthedath@gmail.com>
2013-07-31 20:36:33 +0530

    Added game constants; Introduced Count object

--- src/main/bowling/core/Bowling.java
+++ src/main/bowling/core/Bowling.java
@@ -2,14 +2,16 @@ package bowling.core;
 
  public class Bowling {
     private Frame[] frames;
+    public static final int FRAMES=10;
+    public static final int PINS=10;
 
     public Bowling(Frame[] frames) {
-        this.frames=java.util.Arrays.copyOfRange(frames, 0, Tenpin.TOTAL_FRAMES.value());
+        this.frames=java.util.Arrays.copyOfRange(frames, 0, Bowling.FRAMES);
     }
 
     public void roll(int pins)  {
         for(Frame each : frames)
-            if(each.roll(pins)) return;
+            if(each.roll(new Count(pins))) return;
     }
 
     public int score() {
