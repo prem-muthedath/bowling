e1aba3163136b34ff7582230c79c7906fd8a63be
Prem Muthedath <prem.muthedath@gmail.com>
2013-08-08 17:58:31 +0530

    Moved maxPins code to a method.

--- src/main/bowling/core/PinCount.java
+++ src/main/bowling/core/PinCount.java
@@ -4,23 +4,26 @@ public class PinCount {
 	private int pins;
 
 	public PinCount(int pins) {
-		this(pins, Bowling.PINS);
-	}
-
-	private PinCount(int pins, int maxPins) {
-		set(pins, maxPins);
+		set(pins, maxPins());
 	}
 
 	private void set(int pins, int maxPins) {
 		if(pins < 0 || pins > maxPins) 
-			throw new RuntimeException("EXPECTED PIN COUNT RANGE: 0 - "+maxPins+". FOUND: "+pins+".");
+			throw new RuntimeException("EXPECTED PIN COUNT: 0 - "+maxPins+". FOUND: "+pins+".");
 		this.pins=pins;
+	}
+
+	private int maxPins() {
+		return Bowling.PINS*(1+(pins/Bowling.PINS));
 	}
 
 	public PinCount add(PinCount pinCount) {
 		int sum=this.pins+pinCount.pins;
-		int maxPins=Bowling.PINS*(1+(pins/Bowling.PINS));
-		return new PinCount(sum, maxPins);
+		return new PinCount(sum, maxPins());
+	}
+
+	private PinCount(int pins, int maxPins) {
+		set(pins, maxPins);
 	}
 
 	public Ball next(Ball ball) {
