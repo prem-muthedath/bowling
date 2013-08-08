3a2a912600d1494e13a9757e99067cf83595d0da
Prem Muthedath <prem.muthedath@gmail.com>
2013-08-08 18:20:32 +0530

    New design - deleted PinCount and moved code to Frame.

--- src/main/bowling/core/Frame.java
+++ src/main/bowling/core/Frame.java
@@ -1,23 +1,33 @@
 package bowling.core;
 
 public class Frame {	
-	private PinCount pinCount=new PinCount(0);
+	private int pinfall=0;
 	private Ball ball;
 
 	public Frame(Ball first) {
 		ball=first;
 	}
 
-	public boolean roll(PinCount count) {
+	public boolean roll(int count) {
 		return ball.roll(this, count);
 	}
 
-	void transition(PinCount count) {
-		pinCount=pinCount.add(count);
-		ball=ball.next(pinCount);		
+	void transition(int count) {
+		pinfall=addPins(count);
+		ball=nextBall();		
 	}
 
+	private int addPins(int count) {
+		int sum=pinfall+count;
+		int maxPins=Bowling.PINS*(1+(pinfall/Bowling.PINS));
+		return new PinCountRange(pinfall, maxPins).filter(sum);		
+	}
+
+	private Ball nextBall() {
+		return pinfall==Bowling.PINS  ?  ball.markSuccessor() : ball.nonMarkSuccessor();
+	}	
+
 	public void score(Score score) {
-		ball.score(score, pinCount);
+		ball.score(score, pinfall);
 	}		
 }
