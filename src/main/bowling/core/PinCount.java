84554a42b211c47a577c431ed9ced44ef3611da6
Prem Muthedath <prem.muthedath@gmail.com>
2013-08-09 21:30:09 +0530

    Re-arranged some methods.

--- src/main/bowling/core/PinCount.java
+++ src/main/bowling/core/PinCount.java
@@ -20,19 +20,19 @@ public class PinCount {
 		return new PinSumRange(basePins).filter(sum);
 	}
 
-	public boolean equals(Object object) {
-		return object!=null && this.getClass()==object.getClass() && this.pins==((PinCount) object).pins;
-	}
-
-	public int hashCode() {
-		return pins;
-	}
-
 	Ball nextBall(Ball current) {
 		return  pins==Bowling.PINS  ?  current.markSuccessor() : current.nonMarkSuccessor();
 	}
 
 	public void score(Score score) {
 		score.add(pins);
-	}	
+	}
+
+	public boolean equals(Object object) {
+		return object!=null && this.getClass()==object.getClass() && this.pins==((PinCount) object).pins;
+	}
+
+	public int hashCode() {
+		return pins;
+	}		
 }
