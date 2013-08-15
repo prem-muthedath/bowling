389b82c73a96579f998997534e473dc60483392d
Prem Muthedath <prem.muthedath@gmail.com>
2013-08-15 18:26:34 +0530

    Overrode roll() method to annul transition.

--- src/main/bowling/states/ScoredBall.java
+++ src/main/bowling/states/ScoredBall.java
@@ -1,16 +1,13 @@
 package bowling.states;
 
 import bowling.core.Ball;
+import bowling.core.Frame;
 import bowling.core.PinCount;
 import bowling.core.Score;
 
 class ScoredBall extends Ball {
-	protected PinCount adjust(PinCount pinCount) {
-		return new PinCount();
-	}
-
-	protected Ball nonMarkSuccessor() {
-		return this;
+	public boolean roll(Frame frame, PinCount pinCount) {
+		return rolled();
 	}
 
 	public void score(Score score, PinCount pinCount) {
