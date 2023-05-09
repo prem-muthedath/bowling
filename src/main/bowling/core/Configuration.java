babced0868fd350c35717e1708bb6f2a7aa9c72b
Prem Muthedath <prem.muthedath@gmail.com>
2023-05-09 20:13:19 +0530

    bowling/core/Configuration.java, FrameCount.java: +, edited comments; switched to java /* block comments.

--- src/main/bowling/core/Configuration.java
+++ src/main/bowling/core/Configuration.java
@@ -2,18 +2,19 @@ package bowling.core;
 
 import bowling.states.BallFactory;
 
-/* this object "configures" a bowling game, meaning it does all the work to 
+/**
+ * this object "configures" a bowling game, meaning it does all the work to 
  * create a valid Bowling instance.
  *
  * author: Prem Muthedath
  * */
 class Configuration {
-  // create a new Bowling instance.
+  /* create a new Bowling instance. */
   Bowling newGame() {
     return new Bowling(frames());
   }
 
-  // create all frames in the bowling game.
+  /* create all frames in the bowling game. */
   private Frame[] frames() {
     Frame[] frames=new Frame[Bowling.FRAMES];
     for(int i=0; i < frames.length; i++) {
--- src/main/bowling/core/FrameCount.java
+++ src/main/bowling/core/FrameCount.java
@@ -1,6 +1,7 @@
 package bowling.core;
 
-/* FrameCount, as its name implies, encapsulates a count of frames.
+/**
+ * FrameCount, as its name implies, encapsulates a count of frames.
  * FrameCount knows how to ensure a valid frame count.
  *
  * this tiny class is an excellent example of how you can create a small object 
@@ -16,21 +17,23 @@ public class FrameCount {
     this.count=count; // count of frames
   }
 
-  // compute aggregate score for a valid number of frames.
-  //
-  // note that score is computed for a valid frame count. for example, if you 
-  // had instantiated this object with an invalid frame count of 100, score 
-  // would be computed not for 100 frames but for Bowling.FRAMES, the max 
-  // possible # of frames in a bowling game.
-  //
-  // likewise, if you had created this object with an invalid frame count of 
-  // -20, score would be computed not for -20 but for a valid frame count of 0.
+  /* compute aggregate score for a valid number of frames.
+   *
+   * note that score is computed for a valid frame count. for example, if you 
+   * had instantiated this object with an invalid frame count of 100, score 
+   * would not be computed for 100 frames but for Bowling.FRAMES, the total # of 
+   * frames in a bowling game.
+   *
+   * likewise, if you had created this object with an invalid frame count of 
+   * -20, score would be computed not for -20 but for a valid frame count of 0.
+   */
   public int score(Bowling game) {
     return game.score(validCount());
   }
 
-  // compute a valid frame count.
-  // # of frames >= 0 and <= # of frames allowed in a bowling game.
+  /* compute a valid frame count.
+   * # of frames >= 0 and <= total # of frames allowed in a bowling game.
+   */
   private int validCount() {
     return  this.count < 1   ?   0  :  Math.min(this.count, Bowling.FRAMES);
   }
